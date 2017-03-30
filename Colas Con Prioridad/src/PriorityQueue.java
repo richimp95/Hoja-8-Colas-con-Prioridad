2e5f6d888bea0085dc8b883595d362ba522518ee
richi <richi@172.20.15.219>
2017-03-30 11:37:12 -0600

    "Cambios de codigo"

--- Colas Con Prioridad/src/PriorityQueue.java
+++ Colas Con Prioridad/src/PriorityQueue.java
@@ -1,4 +1,23 @@
 
-public interface PriorityQueue<T> {
-	char prioridad=' ';
+public interface PriorityQueue<E> {
+	public E getFirst();
+	// pre: !isEmpty()
+	// post: returns the minimum value in priority queue
+	
+	public E remove();
+	// pre: !isEmpty()
+	// post: returns and removes minimum value from queue
+	
+	public void add(E value);
+	// pre: value is non-null comparable
+	// post: value is added to priority queue
+	
+	public boolean isEmpty();
+	// post: returns true iff no elements are in queue
+	
+	public int size();
+	// post: returns number of elements within queue
+	
+	public void clear();
+	// post: removes all elements from queue
 }
